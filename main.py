b7b45fccf212b574fa1eedae6998a9bad50fe857
irenekyc <irene@usemanifest.com>
2023-03-22 17:36:31 -0500

    update function

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from llama_index import GPTSimpleVectorIndex
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.environ.get("OPEN_AI_API")
+
+class Prompt(BaseModel):
+    prompt: str
+    indexName: str
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"test": "hello world"}
+    return {"message":"Welcome"}
+
+
+@app.post("/request-response")
+def request_response(prompt: Prompt):
+    index = GPTSimpleVectorIndex.load_from_disk(prompt.indexName)
+    response = index.query(prompt.prompt)
+    return {"prompt": prompt.prompt, "response": response.response}
+
+
